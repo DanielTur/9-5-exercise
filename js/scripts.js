c3edcd85ce00c18995de7ca9688a491d67314850
Daniel Turlinski <daniel.turlinski@gmail.com>
2017-12-04 14:39:28 +0100

    Add 9-5-exercise files

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+var withButtonClass = document.getElementsByClassName('button');
+	
+	for (var i = 0; i < withButtonClass.length; i++) {
+		console.log(withButtonClass[i].innerText);
+	}
